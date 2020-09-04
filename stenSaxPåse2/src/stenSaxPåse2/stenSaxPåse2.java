4b69420b1e7ebe3f6837241b74b47d9be017ec78
davidgalantai <69991333+davidgalantai@users.noreply.github.com>
2020-09-04 11:38:47 +0200

    Update stenSaxPåse2.java

--- stenSaxPåse2/src/stenSaxPåse2/stenSaxPåse2.java
+++ stenSaxPåse2/src/stenSaxPåse2/stenSaxPåse2.java
@@ -4,53 +4,104 @@ import java.util.*;
 
 public class stenSaxPåse2 {
 
+	static final String STEN = "sten";
+	static final String SAX = "sax";
+	static final String PAPPER = "påse";
+
+	static final int ROCK = 1;
+	static final int SCISSOR = 2;
+	static final int PAPER = 3;
+
 	public static void main(String[] args) {
-		System.out.println("Spelare A: sten eller 1, sax eller 2, påse eller 3?"); //programmet skriver ut " "
-		Scanner scan = new Scanner(System.in); //jag skapar en scanner som heter scan
-		String input1 = scan.nextLine(); // jag skapar en sting variable som heter input1. A användaren ange värden.
+		Scanner scan = new Scanner(System.in);
 
-		System.out.println("Spelare B: sten eller 1, sax eller 2, påse eller 3?"); // programmet skriver ut " "
+		System.out.println("welcome to rock paper scissors: Press q to play with words or 1 to play with numbers");
 
-		String input2 = scan.nextLine(); // jag skapar en sting variable som heter input2. B användaren ange värden.
+		String chose = scan.next();
 
-		spel(input1, input2); // jag kallar på spel methoden
-		
+		if (chose.equals("q")) {
+			spel();
+
+		} else {
+
+			spel2();
+
+		}
+
 	}
 
-	public static void spel(String a, String b) { // jag skapar en ny method som heter spel. methoden innehållder string variabeln a och b
+	public static void spel() {
 
-		if (a.equals(b)) { // jag skapar en if påståande som kolla om a värden och b värden är samma.
-			System.out.println("oavgjort!"); // programmet skriver ut oavgjort om a och b är samma.
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Spelare1: Välj Sten, Sax, eller Påse: ");
+		String player1 = sc.next();
+		System.out.println("Spelare 2: Välj Sten, sax, eller Påse: ");
+		String player2 = sc.next();
+
+		if (player1.equals(player2)) {
+			System.out.print("Oavgjort");
+		} else {
+			switch (player1) {
+			case STEN:
+				if (player2.equals(SAX))
+					System.out.print("Spelare 1 vinner med sten!");
+				else
+					System.out.print("Spelare 2 vinner med Påse!");
+				break;
+			case SAX:
+				if (player2.equals(PAPPER))
+					System.out.print("Spelare 1 vinner med sax!");
+				else
+					System.out.print("Spelare 2 vinner sten !");
+				break;
+			case PAPPER:
+				if (player2.equals(STEN))
+					System.out.print("Spelare 1 vinner med påse!");
+				else
+					System.out.print("Spelare 2 vinner med sax!");
+				break;
+
+			}
+
 		}
 
-		if ((a.equals("sten") || b.equals("sten")) && (a.equals("påse") || b.equals("påse"))) { 
-			System.out.println("Påse vinner!");
-		} /*jag skapar en if som kollar om a är lika med sten eller b är lika med sten och om a är lika med påse eller b är lika med påse.
-		    programmet skriver ut att påse vinner */
-		if ((a.equals("sax") || b.equals("sax")) && (a.equals("påse") || b.equals("påse"))) {
-			System.out.println("Sax vinner!");
-		}	/*jag skapar en if som kollar om a är lika med sax eller b är lika med sax och om a är lika med påse eller b är lika med påse.
-	    programmet skriver ut att sax vinner */
-		if ((a.equals("sten") || b.equals("sten")) && (a.equals("sax") || b.equals("sax"))) {
-			System.out.println("Sten vinner!");
-		}   /*jag skapar en if som kollar om a är lika med sten eller b är lika med sten och om a är lika med sax eller b är lika med sax.
-	    programmet skriver ut att sten vinner */
+	}
 
-		if ((a.equals("1") || b.equals("1")) && (a.equals("3") || b.equals("3"))) {
-			System.out.println("Påse vinner!");
-		}	/*jag skapar en if som kollar om a är lika med 1 eller b är lika med 1 och om a är lika med 3 eller b är lika med 3.
-	    programmet skriver ut att påse vinner */
-		if ((a.equals("2") || b.equals("2")) && (a.equals("3") || b.equals("3"))) {
-			System.out.println("Sax vinner!");
-		}	/*jag skapar en if som kollar om a är lika med 2 eller b är lika med 2 och om a är lika med 3 eller b är lika med 3.
-	    programmet skriver ut att sax vinner */
-		if ((a.equals("1") || b.equals("1")) && (a.equals("2") || b.equals("2"))) {
-			System.out.println("Sten vinner!");
-			/*jag skapar en if som kollar om a är lika med 1 eller b är lika med 1 och om a är lika med 2 eller b är lika med 2.
-		    programmet skriver ut att sten vinner */
-		
-			
+	public static void spel2() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Spelare1: Välj (1) för Sten, (2) för Sax, eller (3) för Påse: ");
+		int player1 = sc.nextInt();
+		System.out.println("Spelare 2: Välj (1) för Sten, (2) för sax, eller (3) för Påse: ");
+		int player2 = sc.nextInt();
+
+		if (player1 == (player2)) {
+			System.out.print("Oavgjort");
+		} else {
+			switch (player1) {
+			case ROCK:
+				if (player2 == (SCISSOR))
+					System.out.print("Spelare 1 vinner med sten!");
+				else
+					System.out.print("Spelare 2 vinner med Påse!");
+				break;
+			case SCISSOR:
+				if (player2 == (PAPER))
+					System.out.print("Spelare 1 vinner med sax!");
+				else
+					System.out.print("Spelare 2 vinner sten !");
+				break;
+			case PAPER:
+				if (player2 == (ROCK))
+					System.out.print("Spelare 1 vinner med påse!");
+				else
+					System.out.print("Spelare 2 vinner med sax!");
+				break;
+
+			}
+
 		}
-		
+
 	}
 }
